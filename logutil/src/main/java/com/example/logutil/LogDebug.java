53f04af2b0bd4127ea3a66628ecfb571a9b57630
MahmoudMostafa125 <mahmoudmostafa125125@yahoo.com>
2019-03-14 14:44:27 +0200

    k

---
+++ logutil/src/main/java/com/example/logutil/LogDebug.java
@@ -0,0 +1,9 @@
+package com.example.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+    public static  void d(String mesg){
+        Log.e("HEY","First Lib for @@@@@"+mesg);
+    }
+}
